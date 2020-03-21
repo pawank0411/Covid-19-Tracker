674fd92e30545458a60c69351bb1c4f6064acc89
pawangorai <pawankumar17264@gmail.com>
2020-03-21 22:39:30 +0530

    added about page, fixed bugs

---
+++ app/src/main/java/india/coronavirus/fight/About.java
@@ -0,0 +1,59 @@
+package india.coronavirus.fight;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class About extends AppCompatActivity {
+    private boolean update;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        MaterialTextView check = findViewById(R.id.update_available);
+        MaterialTextView pawan = findViewById(R.id.pawan);
+        MaterialTextView sppedx = findViewById(R.id.speedx);
+        CollectionReference apiCollection = FirebaseFirestore.getInstance().collection("update");
+        apiCollection.addSnapshotListener((queryDocumentSnapshots, e) -> {
+            if (queryDocumentSnapshots != null) {
+                for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
+                    update = documentChange.getDocument().getBoolean("available");
+                    if (update) {
+                        check.setVisibility(View.VISIBLE);
+                        check.setText(Html.fromHtml("<u>Update Available : </u>"));
+                    }
+
+                }
+            }
+        });
+        pawan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://pawan0411.github.io/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        sppedx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/TheSpeedX");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/india/coronavirus/fight/MainActivity.java
+++ app/src/main/java/india/coronavirus/fight/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.MenuCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,6 +23,8 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.onesignal.OneSignal;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
@@ -73,6 +79,24 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            }).show();
 //        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav, menu);
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+        MenuCompat.setGroupDividerEnabled(menu, true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.about_app) {
+            Intent intent = new Intent(this, About.class);
+            startActivity(intent);
+            return (true);
+        }
+        return (super.onOptionsItemSelected(item));
     }
 
     @Override
