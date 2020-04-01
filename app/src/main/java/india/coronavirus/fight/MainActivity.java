8ab2ce73e0314d377147544be1686eced383f733
Pawan0411 <pawankumar17264@gmail.com>
2020-04-01 18:39:18 +0530

    lint fix

--- app/src/main/java/india/coronavirus/fight/MainActivity.java
+++ app/src/main/java/india/coronavirus/fight/MainActivity.java
@@ -1,6 +1,7 @@
 package india.coronavirus.fight;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -81,13 +82,12 @@ public class MainActivity extends AppCompatActivity {
         downloadDistrictFile.newDownload(sharedPreferences.getString("DIST", ""));
 
         RequestQueue requestQueue = Volley.newRequestQueue(getApplication());
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, sharedPreferences.getString("API", "http://ac41bf31.ngrok.io") + "/api/extras", response -> {
+        @SuppressLint("SetTextI18n") StringRequest stringRequest = new StringRequest(Request.Method.GET, sharedPreferences.getString("API", "http://ac41bf31.ngrok.io") + "/api/extras", response -> {
             try {
                 JSONObject json = new JSONObject(response);
                 lastUp = json.getString("timestamp");
                 editor.putString("time", lastUp);
                 editor.apply();
-                sharedPreferences.getString("time", "null");
                 if (!Objects.equals(sharedPreferences.getString("time", null), "")) {
                     lastup.setText("Last updated " + sharedPreferences.getString("time", "null").substring(3));
                 }
