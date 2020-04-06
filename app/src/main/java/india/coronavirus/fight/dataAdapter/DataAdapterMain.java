d3ade4b9b6f5b01e71fc1149a86aadc9b87d0fe5
Pawan0411 <pawankumar17264@gmail.com>
2020-04-06 21:10:54 +0530

    added screenShots

--- app/src/main/java/india/coronavirus/fight/dataAdapter/DataAdapterMain.java
+++ app/src/main/java/india/coronavirus/fight/dataAdapter/DataAdapterMain.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
